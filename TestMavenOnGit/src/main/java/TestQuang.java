b1d48e0a82230b64ca20bf420bb6f83169468d1d
Quang Tran <qtran@pingidentity.com>
2015-08-28 11:35:33 +0700

    more

---
+++ TestMavenOnGit/src/main/java/TestQuang.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class TestQuang {
+	@Test
+	public void test1() {
+		String str = "TestNG is working fine";
+		assertEquals("TestNG is working fine", str);
+	}
+	
+	@Test
+	public void test2() {
+		String str = "TestNG is working fine";
+		assertEquals("TestNG is working fine", str);
+	}
+}
